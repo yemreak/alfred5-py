13968248d9a0e857ee6b7239929565324e7a791c
yedhrab <yedhrab@gmail.com>
2023-04-11 03:40:56 +0300

    insignificant updates

--- setup.py
+++ setup.py
@@ -1,18 +1,42 @@
+"""Setup script for package
+
+References: 
+    - https://github.com/openai/openai-python/blob/main/setup.cfg
+"""
+
 from glob import glob
 from os.path import basename, splitext
 from setuptools import find_packages, setup
 from pathlib import Path
 
 VERSION = "1.1.0"
+DESCRIPTION = "Simple python wrapper for alfred5 workflow / snippets"
 README_PATH = "docs/README.md"
 USERNAME = "yedhrab"
 REPOSITORY = "alfred5"
+KEYWORDS = [
+    "alfred",
+    "alfred5",
+    "workflow",
+    "snippets",
+    "alfred-workflows",
+    "alfred-snippets",
+]
+INSTALL_REQUIRES = ["aiohttp==3.8.4", "ruamel.yaml==0.17.21"]
+EXSTRAS_REQUIRE = {
+    "dev": [
+        "black==23.3.0",
+        "autoflake==2.0.2",
+        "pytest==7.3.0",
+        "pytest-asyncio==0.21.0",
+    ]
+}
 
 setup(
     name=REPOSITORY,
     version=VERSION,
     license="Apache Software License 2.0",
-    description="Simple python wrapper for alfred workflow / snippets",
+    description=DESCRIPTION,
     long_description=Path(README_PATH).read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
     author="Yunus Emre Ak",
@@ -25,10 +49,8 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: Unix",
-        "Operating System :: POSIX",
-        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
@@ -36,21 +58,13 @@ setup(
     ],
     project_urls={
         "Source": f"https://github.com/{USERNAME}/{REPOSITORY}/",
-        "Documentation": f"https://github.com/{USERNAME}/{REPOSITORY}/wiki",
         "Changelog": f"https://github.com/{USERNAME}/{REPOSITORY}/releases",
         "Issue Tracker": f"https://github.com/{USERNAME}/{REPOSITORY}/issues",
     },
-    keywords=[
-        "alfred",
-        "alfred5",
-        "workflow",
-        "snippets",
-        "alfred-workflows",
-        "alfred-snippets",
-    ],
+    keywords=KEYWORDS,
     python_requires=">=3.9.6",
-    install_requires=["aiohttp==3.8.3", "ruamel.yaml==0.17.21"],
-    extras_require={},
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXSTRAS_REQUIRE,
     setup_requires=[],
     entry_points={},
 )
